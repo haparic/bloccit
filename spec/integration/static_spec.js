66e010d1ab636b9b526be395083303d26ac8da70
Hugo Aparicio <hugo.aparicio8@gmail.com>
2019-08-11 20:33:54 -0700

    f1

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -5,15 +5,15 @@ const base = "http://localhost:3000/";
 describe("routes : static", () => {
 
 //#1
-  describe("GET /", () => {
+  describe("GET /marco", () => {
 
 //#2
-    it("should return status code 200", (done) => {
+    it("should return status code 200 and return 'Polo' after 'Marco'", (done) => {
 
 //#3
-      request.get(base, (err, res, body) => {
+      request.get(base+'marco', (err, res, body) => {
         expect(res.statusCode).toBe(200);
-
+        expect(body).toContain("Polo");
 //#4
         done();
       });
